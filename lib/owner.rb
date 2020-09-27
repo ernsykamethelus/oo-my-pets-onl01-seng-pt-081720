870293cbe979e3953c1cd70caf84d10086212c75
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 02:10:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Owner
-  # code goes here
-end
+  attr_accessor :name
+  
+    def initialize(name)
+        @name = name
+      end
+      
+      def cannot_change_owner_name(name)
+        binding.pry
+    raise NoMethodError, "#{owner.name}" if !owner.is_a?(Owner)
+  end
+    end
+      
